7ef8cc0067b4eb2922c16b490a1926d87a76c3f9
Flatn3ck <74960799+Flatn3ck@users.noreply.github.com>
2021-04-20 20:17:27 +0200

    Update Program.cs

--- Reserveringssysteem/Program.cs
+++ Reserveringssysteem/Program.cs
@@ -10,7 +10,7 @@ namespace Reserveringssysteem
             Serialize(new Table[] { new Table("1", 6, new Customer("Oscar"), new Bill(0.00)), new Table("2", 4, new Customer("Arjan"), new Bill(100.0)) }, "tables.json");
             Serialize(new Restaurant[] { new Restaurant(
                 "Restaurant De Houten Vork",
-                @"Kom gezellig eten!",
+                @"Kom gezellig eten!!",
                 new string[]{ "Wijnhaven 107", "3011WH", "Rotterdam"},
                 new string[]{ "Maandag 14:00 - 22:00",
                 "Dinsdag 14:00 - 22:00",
